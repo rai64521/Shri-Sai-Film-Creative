4561c45accca6ade64648a1028fbc5b691b1a412
rai64521 <raisumant99@gmail.com>
2025-09-05 14:27:12 +0530

    react redux toolkit

---
+++ reduxToolKitTodo/src/App.jsx
@@ -0,0 +1,17 @@
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Learn about Redux ToolKit </h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
